c09e0817286123c9af5383bcd462d1109985f8f3
ErikBuer <buer94@gmail.com>
2021-08-18 21:32:29 +0200

    Moved to i32 only

--- src/vector.rs
+++ src/vector.rs
@@ -3,39 +3,28 @@
 
 
 /// Create vector type of size N and type T.
-macro_rules! declare_type{
+macro_rules! declare_type_real{
     ( $name:ident, $N:expr) => {
-        /// Numeric vector of type T.
-        /// # Examples
-        /// ```
-        /// use numeric_vector::vector::$name;
-        /// let x = $name{ len:4, data: [0,0,0,0] };
-        /// assert_eq!{x.len, 4};
-        /// ```
-        pub struct $name<T> {
+        /// Real numeric vector of type int32.
+        pub struct $name{
             pub len: usize,
-            pub data: [T; $N],
+            pub data: [i32; $N],
         }
 
     };
 }
 
+declare_type_real!( scalar, 1);
 
-
-declare_type!( Vec4, 4);
-
-/*
 #[cfg(test)]
 mod tests {
-    use crate::*;
+    //use crate::*;
     use super::*;
 
     #[test]
-    fn cv_trait_add() {
-        let vec = vec![ C32F!(2,0), C32F!(0,4), C32F!(-2,0) ];
-        let vec_obj = VecC32F::new(vec);
-        let sum = &vec_obj+&vec_obj;
-        assert_eq!( VecC32F::new(vec![ C32F!(4,0), C32F!(0,8), C32F!(-4,0) ]), sum );
+    fn test_scalar_len() {
+        let x = $name{ len:4, data: [0,0,0,0] };
+        assert_eq!{x.len, 4};
     }
 }
-*/
+
